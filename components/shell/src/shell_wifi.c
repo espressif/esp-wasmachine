54ab84eba95db18cd79ba7fb7a1c6e3a6122fb38
Dong Heng <dongheng@espressif.com>
2022-12-14 17:03:02 +0800

    Fix some issues:
    1. Not set sta mode before configuring Wi-Fi
    2. esptool download fails in case of filling all 16MB flash

--- components/shell/src/shell_wifi.c
+++ components/shell/src/shell_wifi.c
@@ -60,6 +60,7 @@ static int sta_func(int argc, char **argv)
     }
 
     if (strlen((char *)wifi_config.sta.ssid)) {
+        ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
         ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
         ESP_ERROR_CHECK(esp_wifi_start());
     } else {
